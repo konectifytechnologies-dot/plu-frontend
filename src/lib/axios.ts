67d828159341625e3acaa14873ff02c14565ba9d
Peter mwangi <konectifytechnologies@gmail.com>
2026-02-24 16:33:48 +0300

    updated design

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,5 +3,7 @@ import axios from "axios";
 export default axios.create({
     baseURL:'https://api.pludevelopers.co.ke',
     withCredentials:true,
-    withXSRFToken:true
+    withXSRFToken:true,
+    xsrfCookieName: 'XSRF-TOKEN',
+    xsrfHeaderName: 'X-XSRF-TOKEN',
 });
